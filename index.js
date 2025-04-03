cbbdf00fa8faf5a68c1536f0a1b8b10a2e67b822
Oz <beatriceb7890@gmail.com>
2025-04-03 08:33:05 +0300

    commit initial

---
+++ index.js
@@ -0,0 +1,22 @@
+// Single line comment
+
+/*
+Multi
+line  
+comment 
+*/
+
+// Failo issaugojimas: CTR + s
+
+console.log(322);
+console.log(2);
+console.log(333);
+console.log(2 * 2 + 2);
+
+console.log(1, 2, 3);
+
+console.log('labas rytas, Lietuva');
+
+console.log('Protinga matematika:','2*2+2=',2*2+2);
+
+console.log('man yra',21);
